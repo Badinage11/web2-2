76b1cafb3d26fc74292ab5c5b1bd49bde93b1c86
Badinage11 <147810289+Badinage11@users.noreply.github.com>
2024-09-22 21:12:16 +0800

    Get all active fundraising activities, including categories

--- concertAPI/controllerAPI/api-controller.js
+++ concertAPI/controllerAPI/api-controller.js
@@ -8,64 +8,48 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get("/", (req, res)=>{
-	connection.query("select * from FUNDRAISER", (err, records,fields)=> {
-		 if (err){
-			 console.error("Error while retrieve the data");
-		 }else{
-			 res.send(records);
-		 }
-	})
-})
+// router.get("/", (req, res)=>{
+// 	connection.query("select * from FUNDRAISER", (err, records,fields)=> {
+// 		 if (err){
+// 			 console.error("Error while retrieve the data");
+// 		 }else{
+// 			 res.send(records);
+// 		 }
+// 	})
+// })
 
-router.get("/:id", (req, res)=>{
-	connection.query("select * from concert where ConcertID=" + req.params.id, (err, records,fields)=> {
-		 if (err){
-			 console.error("Error while retrieve the data");
-		 }else{
-			 res.send(records);
-		 }
-	})
-})
+// router.get("/category", (req, res)=>{
+// 	connection.query("select * from CATEGORY", (err, records,fields)=> {
+// 		 if (err){
+// 			 console.error("Error while retrieve the data");
+// 		 }else{
+// 			 res.send(records);
+// 		 }
+// 	})
+// })
 
-router.post("/", (req, res)=>{
-	console.log(req.body.ConcertID);
-	console.log(req.body.ArtistName);	
-	console.log(req.body.Location);
-	var ConcertID = req.body.ConcertID;
-	var ArtistName = req.body.ArtistName;
-	var Location = req.body.Location;
-	connection.query("INSERT INTO concert VALUES("+ConcertID+",'"+ArtistName+"','"+Location+"')",
-	(err, result)=> {
-		 if (err){
-			 console.error("Error while retrieve the data" + err);
-		 }else{
-			 res.send({insert:"success"});
-		 }
-	})
-})
 
-router.put("/", (req, res)=>{
-	var ConcertID = req.body.ConcertID;
-	var Location = req.body.Location;
-	connection.query("UPDATE concert SET Location='"+Location+"' where ConcertID="+ConcertID,
-	(err, result)=> {
-		 if (err){
-			 console.error("Error while Updating the data" + err);
-		 }else{
-			 res.send({update:"success"});
-		 }
-	})
-})
 
-router.delete("/:id", (req, res)=>{
-	connection.query("delete from concert where ConcertID=" + req.params.id, (err, records,fields)=> {
-		 if (err){
-			 console.error("Error while deleting the data");
-		 }else{
-			 res.send({delete:"Delete Sucess"});
-		 }
-	})
-})
+router.get("/fundraisers", (req, res) => {
+	const query = `
+	  SELECT f.*, c.NAME AS category_name
+	  FROM FUNDRAISER f
+	  JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
+	  WHERE f.ACTIVE = 1
+	`;
+	
+	connection.query(query, (err, records, fields) => {
+	  if (err) {
+		console.error("Error while retrieving the data:", err);
+		res.status(500).json({ error: 'Internal server error' });
+	  } else {
+		res.send(records);
+	  }
+	});
+  });
+  
+  
+  
+
 
 module.exports = router;
--- concertAPI/server.js
+++ concertAPI/server.js
@@ -12,7 +12,7 @@ var bodyparser=require("body-parser");
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 
-app.use("/api/concerts", concertAPI);
+app.use("/api/fundraiser", concertAPI);
 
 
 app.listen(3060);
