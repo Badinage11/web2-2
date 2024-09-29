6e6840c496ceb8591529bfa89c4b36be995655be
Badinage11 <147810289+Badinage11@users.noreply.github.com>
2024-09-29 23:30:47 +0800

    Further modified the code and added comments to some parts.

--- concertAPI/controllerAPI/api-controller.js
+++ concertAPI/controllerAPI/api-controller.js
@@ -1,309 +1,14 @@
-// var dbcon = require("../crowdfunding_db");
-
-// var connection = dbcon.getconnection();
-
-// connection.connect();
-
-// var express = require('express');
-
-// var router = express.Router();
-
-
-// //Get all active fundraising activities, including categories
-// router.get("/fundraisers", (req, res) => {
-// 	const query = `
-// 	  SELECT f.*, c.NAME AS category_name
-// 	  FROM FUNDRAISER f
-// 	  JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
-// 	  WHERE f.ACTIVE = 1
-// 	`;
-	
-// 	connection.query(query, (err, records, fields) => {
-// 	  if (err) {
-// 		console.error("Error while retrieving the data:", err);
-		
-// 	  } else {
-// 		res.send(records);
-// 	  }
-// 	});
-//   });
-
-
-  
-//   //Get all categories:
-//   router.get("/categories", (req, res) => {
-// 	const query = 'SELECT * FROM CATEGORY';
-	
-// 	connection.query(query, (err, records, fields) => {
-// 	  if (err) {
-// 		console.error("Error while retrieving the data:", err);
-		
-// 	  } else {
-// 		res.send(records);
-// 	  }
-// 	});
-//   });
-  
-
-//   //Search for fundraising activities that meet relevant criteria
-//   router.get("/search", (req, res) => {
-// 	const { CATEGORY, CITY, ORGANIZER } = req.query;
-// 	let sql = "SELECT f.*, c.NAME AS category_name FROM FUNDRAISER f JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID WHERE f.ACTIVE = 1";
-// 	let params = [];
-  
-// 	if (CATEGORY || CITY || ORGANIZER) {
-// 	  if (CATEGORY) {
-// 		sql += " AND c.CATEGORY_ID = ?";
-// 		params.push(CATEGORY);
-// 	  }
-// 	  if (CITY) {
-// 		sql += " AND f.CITY LIKE ?";
-// 		params.push("%" + CITY + "%");
-// 	  }
-// 	  if (ORGANIZER) {
-// 		sql += " AND f.ORGANIZER LIKE ?";
-// 		params.push("%" + ORGANIZER + "%");
-// 	  }
-// 	}
-  
-// 	connection.query(sql, params, (err, results) => {
-// 	  if (err) {
-// 		console.log("Query error occurred:", err);
-		
-// 	  } else {
-// 		res.send(records);
-// 	  }
-// 	});
-//   });
-  
-
-
-
-//   //Query detailed information of activities through ID
-//   router.get("/fundraiser/:id", (req, res) => {
-// 	const { id } = req.params;
-// 	const query = `
-// 	  SELECT f.*, c.NAME AS category_name
-// 	  FROM FUNDRAISER f
-// 	  JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
-// 	  WHERE f.FUNDRAISER_ID = ?
-// 	`;
-	
-// 	connection.query(query, [id], (err, records, fields) => {
-// 	  if (err) {
-// 		console.error("Error while retrieving the data:", err);
-// 	} else {
-// 		res.send(records);
-// 	  }
-// 	});
-//   });
-  
-
-
-
-// // module.exports = router;
-// var dbcon = require("../crowdfunding_db");
-// var connection = dbcon.getconnection();
-// connection.connect();
-
-// var express = require('express');
-// var router = express.Router();
-
-// // 获取所有活跃的募款活动，包括类别
-// router.get("/fundraisers", (req, res) => {
-//   const query = `
-//     SELECT f.*, c.NAME AS category_name
-//     FROM FUNDRAISER f
-//     JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
-//     WHERE f.ACTIVE = 1
-//   `;
-//   connection.query(query, (err, records) => {
-//     if (err) {
-//       console.error("检索数据时出错:", err);
-//       res.status(500).send("检索数据时出错");
-//     } else {
-//       res.send(records);
-//     }
-//   });
-// });
-
-// // 获取所有类别
-// router.get("/categories", (req, res) => {
-//   const query = 'SELECT * FROM CATEGORY';
-//   connection.query(query, (err, records) => {
-//     if (err) {
-//       console.error("检索数据时出错:", err);
-//       res.status(500).send("检索数据时出错");
-//     } else {
-//       res.send(records);
-//     }
-//   });
-// });
-
-// // 根据条件搜索募款活动
-// router.get("/search", (req, res) => {
-//   const { CATEGORY, CITY, ORGANIZER } = req.query;
-//   let sql = "SELECT f.*, c.NAME AS category_name FROM FUNDRAISER f JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID WHERE f.ACTIVE = 1";
-//   let params = [];
-
-//   if (CATEGORY || CITY || ORGANIZER) {
-//     if (CATEGORY) {
-//       sql += " AND c.CATEGORY_ID = ?";
-//       params.push(CATEGORY);
-//     }
-//     if (CITY) {
-//       sql += " AND f.CITY LIKE ?";
-//       params.push("%" + CITY + "%");
-//     }
-//     if (ORGANIZER) {
-//       sql += " AND f.ORGANIZER LIKE ?";
-//       params.push("%" + ORGANIZER + "%");
-//     }
-//   }
-
-//   connection.query(sql, params, (err, results) => {
-//     if (err) {
-//       console.log("查询时出错:", err);
-//       res.status(500).send("查询时出错");
-//     } else {
-//       res.send(results);
-//     }
-//   });
-// });
-
-// // 通过 ID 查询活动详细信息
-// router.get("/fundraiser/:id", (req, res) => {
-//   const { id } = req.params;
-//   const query = `
-//     SELECT f.*, c.NAME AS category_name
-//     FROM FUNDRAISER f
-//     JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
-//     WHERE f.FUNDRAISER_ID = ?
-//   `;
-//   connection.query(query, [id], (err, records) => {
-//     if (err) {
-//       console.error("检索数据时出错:", err);
-//       res.status(500).send("检索数据时出错");
-//     } else {
-//       res.send(records);
-//     }
-//   });
-// });
-
-// module.exports = router;
-
-// var dbcon = require("../crowdfunding_db");
-
-// var connection = dbcon.getconnection();
-
-// connection.connect();
-
-// var express = require('express');
-
-// var router = express.Router();
-
-
-// //Get all active fundraising activities, including categories
-// router.get("/fundraisers", (req, res) => {
-// 	const query = `
-// 	  SELECT f.*, c.NAME AS category_name
-// 	  FROM FUNDRAISER f
-// 	  JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
-// 	  WHERE f.ACTIVE = 1
-// 	`;
-	
-// 	connection.query(query, (err, records, fields) => {
-// 	  if (err) {
-// 		console.error("Error while retrieving the data:", err);
-		
-// 	  } else {
-// 		res.send(records);
-// 	  }
-// 	});
-//   });
-
-
-  
-//   //Get all categories:
-//   router.get("/categories", (req, res) => {
-// 	const query = 'SELECT * FROM CATEGORY';
-	
-// 	connection.query(query, (err, records, fields) => {
-// 	  if (err) {
-// 		console.error("Error while retrieving the data:", err);
-		
-// 	  } else {
-// 		res.send(records);
-// 	  }
-// 	});
-//   });
-  
-
-//   //Search for fundraising activities that meet relevant criteria
-//   router.get("/search", (req, res) => {
-// 	const { CATEGORY, CITY, ORGANIZER } = req.query;
-// 	let sql = "SELECT f.*, c.NAME AS category_name FROM FUNDRAISER f JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID WHERE f.ACTIVE = 1";
-// 	let params = [];
-  
-// 	if (CATEGORY || CITY || ORGANIZER) {
-// 	  if (CATEGORY) {
-// 		sql += " AND c.CATEGORY_ID = ?";
-// 		params.push(CATEGORY);
-// 	  }
-// 	  if (CITY) {
-// 		sql += " AND f.CITY LIKE ?";
-// 		params.push("%" + CITY + "%");
-// 	  }
-// 	  if (ORGANIZER) {
-// 		sql += " AND f.ORGANIZER LIKE ?";
-// 		params.push("%" + ORGANIZER + "%");
-// 	  }
-// 	}
-  
-// 	connection.query(sql, params, (err, results) => {
-// 	  if (err) {
-// 		console.log("Query error occurred:", err);
-		
-// 	  } else {
-// 		res.send(records);
-// 	  }
-// 	});
-//   });
-  
-
-
-
-//   //Query detailed information of activities through ID
-//   router.get("/fundraiser/:id", (req, res) => {
-// 	const { id } = req.params;
-// 	const query = `
-// 	  SELECT f.*, c.NAME AS category_name
-// 	  FROM FUNDRAISER f
-// 	  JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
-// 	  WHERE f.FUNDRAISER_ID = ?
-// 	`;
-	
-// 	connection.query(query, [id], (err, records, fields) => {
-// 	  if (err) {
-// 		console.error("Error while retrieving the data:", err);
-// 	} else {
-// 		res.send(records);
-// 	  }
-// 	});
-//   });
-  
-
-
-
-// module.exports = router;
+// Import the database connection module
 var dbcon = require("../crowdfunding_db");
+// Get a connection from the database connection module
 var connection = dbcon.getconnection();
 connection.connect();
 
+// Import the Express framework
 var express = require('express');
 var router = express.Router();
 
-// 获取所有活跃的募款活动，包括类别
+//Get all active fundraising activities, including categories
 router.get("/fundraisers", (req, res) => {
   const query = `
     SELECT f.*, c.NAME AS category_name
@@ -313,33 +18,36 @@ router.get("/fundraisers", (req, res) => {
   `;
   connection.query(query, (err, records) => {
     if (err) {
-      console.error("检索数据时出错:", err);
-      res.status(500).send("检索数据时出错");
+      console.error("Error retrieving data:", err);
+      res.status(500).send("Error retrieving data");
     } else {
       res.send(records);
     }
   });
 });
 
-// 获取所有类别
+// Route to get all categories
 router.get("/categories", (req, res) => {
   const query = 'SELECT * FROM CATEGORY';
+  // Execute the SQL query
   connection.query(query, (err, records) => {
     if (err) {
-      console.error("检索数据时出错:", err);
-      res.status(500).send("检索数据时出错");
+      console.error("Error retrieving data:", err);
+      res.status(500).send("Error retrieving data");
     } else {
       res.send(records);
     }
   });
 });
 
-// 根据条件搜索募款活动
+
+// Search for fundraising activities based on criteria
 router.get("/search", (req, res) => {
   const { CATEGORY, CITY, ORGANIZER } = req.query;
   let sql = "SELECT f.*, c.NAME AS category_name FROM FUNDRAISER f JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID WHERE f.ACTIVE = 1";
   let params = [];
 
+  // Add conditions based on query parameters
   if (CATEGORY || CITY || ORGANIZER) {
     if (CATEGORY) {
       sql += " AND c.CATEGORY_ID = ?";
@@ -355,17 +63,18 @@ router.get("/search", (req, res) => {
     }
   }
 
+  // Execute the SQL query with parameters
   connection.query(sql, params, (err, results) => {
     if (err) {
-      console.log("查询时出错:", err);
-      res.status(500).send("查询时出错");
+      console.log("Error occurred during query:", err);
+      res.status(500).send("Error occurred during query");
     } else {
       res.send(results);
     }
   });
 });
 
-// 通过 ID 查询活动详细信息
+// Route to get fundraiser details by ID
 router.get("/fundraiser/:id", (req, res) => {
   const { id } = req.params;
   const query = `
@@ -374,14 +83,17 @@ router.get("/fundraiser/:id", (req, res) => {
     JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
     WHERE f.FUNDRAISER_ID = ?
   `;
+
+  // Execute the SQL query with the fundraiser ID
   connection.query(query, [id], (err, records) => {
     if (err) {
-      console.error("检索数据时出错:", err);
-      res.status(500).send("检索数据时出错");
+      console.error("Error retrieving data:", err);
+      res.status(500).send("Error retrieving data");
     } else {
       res.send(records);
     }
   });
 });
 
+// Export the router to be used in other parts of the application
 module.exports = router;
--- concertAPI/server.js
+++ concertAPI/server.js
@@ -1,15 +1,18 @@
-var express = require('express');
+var express = require('express');// Import the Express framework
 const cors = require("cors"); // Import cors
 
+// Create an instance of an Express application
 var app = express();
-// Enable CORS (must after initialise it)
 app.use(cors());
 
+// Import the API controller module for handling specific routes
 var concertAPI = require("./controllerAPI/api-controller");
 
 var bodyparser=require("body-parser");
 
+// Use Body-Parser to parse JSON requests
 app.use(bodyparser.json());
+// Use Body-Parser to parse URL-encoded requests with extended syntax
 app.use(bodyparser.urlencoded({extended:false}));
 
 app.use("/api/fundraiser", concertAPI);
@@ -17,4 +20,5 @@ app.use("/api/fundraiser", concertAPI);
 
 app.listen(3060);
 
+// Log a message to the console indicating that the server is running
 console.log("Server up and running on port 3060");
